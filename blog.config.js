26c52331a907bb1359f213fca489b5a37cc22a12
redzhx <115926854+redzhx@users.noreply.github.com>
2024-02-21 15:54:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是勤奋的懒人，在思考的海洋中划水。我是博学的无知者，在知识的沙滩上晒太阳。我是专注的拖延者，在效率的道路上散步。我是矛盾的统一者，在浅薄中追求深度。欢迎来到我的博客🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，欢迎来到我的博客🎉,在思考的海洋中划水,在知识的沙滩上晒太阳,在效率的道路上散步,在浅薄中追求深度。'
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
